ba52a01f87a2dcf31e819c0f91a894f9587c69c2
christophe Lhermitte <clhermit@toulouse.viveris.com>
2018-02-14 10:50:36 +0100

    First release

---
+++ rest/index.php
@@ -0,0 +1,62 @@
+<?php
+
+require 'slim/vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+//$settings = require 'slim/src/settings.php';
+$settings= [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => 'slim/templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => 'slim/logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+    ],
+];
+
+
+$app = new \Slim\App($settings);
+
+// Define app routes
+$app->get('/hello', function () {
+    echo "Hello, I'm Slim";
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app->get('/data', function(ServerRequestInterface $request, ResponseInterface $response) {
+    $data = array('name' => 'Bob', 'age' => 40);
+    $newResponse = $response->withJson($data);
+    return $newResponse;
+});
+
+// Set up dependencies
+require 'slim/src/dependencies.php';
+
+// Register middleware
+require 'slim/src/middleware.php';
+
+// Register routes
+require 'slim/src/routes.php';
+
+
+// Run app
+$app->run();
+
+?>
